42fc8667c3b79c4643b833132ab2189853f655b5
Beatrice Lee <beatricelee@Beatrices-MacBook-Pro.local>
2020-12-09 01:12:41 +0900

    db bugfix

--- Code/db/Database.py
+++ Code/db/Database.py
@@ -3,9 +3,9 @@ import os
 
 class Database():
     def __init__(self):
-        host = os.environ['MYSQLHost']
-        user = os.environ['MYSQLName']
-        password = os.environ['MYSQLPwd']
+        host = "35.236.198.39"
+        user = "root"
+        password = "plan2020"
         db_name = "planning"
         self.db = pymysql.connect(
             user=user, 
@@ -29,13 +29,13 @@ class Database():
 
     def insertAnswers(self, values):
         cursor = self.db.cursor(pymysql.cursors.DictCursor)
-        sql = "INSERT INTO answers (user_name, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}', '{12}', '{13}', '{14}', '{15}', '{16}', '{17}', '{18}', '{19}', '{20}');".format(*values)
+        sql = "INSERT INTO answers (user_name, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}', '{12}', '{13}', '{14}', '{15}', '{16}', '{17}', '{18}', '{19}', '{20}', '{21}');".format(*values)
         cursor.execute(sql)
         self.db.commit()
 
     def insertEval(self, values):
         cursor = self.db.cursor(pymysql.cursors.DictCursor)
-        sql = "INSERT INTO feedbacks (user_name, evaluation) VALUES ('{0}', {1});".format(*values)
+        sql = "INSERT INTO evaluations (user_name, evaluation) VALUES ('{0}', {1});".format(*values)
         cursor.execute(sql)
         self.db.commit()
 
