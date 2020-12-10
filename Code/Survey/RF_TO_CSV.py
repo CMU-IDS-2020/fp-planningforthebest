b347d3e6ca6525bd494a7eae0a599e48f57b1d4b
Weiqin Wang <weiqinw@andrew.cmu.edu>
2020-12-10 15:38:01 -0500

    clean code

--- Code/Survey/RF_TO_CSV.py
+++ Code/Survey/RF_TO_CSV.py
@@ -3,17 +3,11 @@ from sklearn.ensemble.forest import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 from sklearn.model_selection import train_test_split
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.datasets import make_classification
 from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.inspection import permutation_importance
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
-'''
-Author Weiqin Wang
-date 07/13/2019  part of script developed during NSF REU CAAR 2019 DICKERSON GROUP
-'''
 features = {
 "0":"Paralysis ", # partial body paralysis
 "1":"Voice ",
@@ -36,8 +30,8 @@ header = [  "Paralysis ",
 			"Incontinence " ,
 			"Emotions ",
 			 "Sex " ]
+			 
 #feature importance plot 
-
 def RF_Features_Importance(X,Y,outputfile="RF.csv"):
 	forest = RandomForestClassifier(n_estimators= 300)
 	forest.fit(X, Y)
@@ -47,24 +41,4 @@ def RF_Features_Importance(X,Y,outputfile="RF.csv"):
                   columns = ["Features","Importance"]) 
 
 	df.to_csv(outputfile,index=False)
-'''
-def RF_Features_Importance(X,Y,outputfile="RF.csv"):
-	model = LogisticRegression(C=1e5, solver='lbfgs').fit(X, Y)
 
-
-	r = permutation_importance(model, X, Y, n_repeats=30 ,random_state=0)
-	print(r)
-
-	df = pd.DataFrame(list(zip(header,importances)),
-             
-                  columns = ["Features","Importance"]) 
-
-	df.to_csv(outputfile,index=False)
-'''
-#Testing 
-"""
-total = pd.read_csv("Survey_Results.csv")
-X = total.iloc[:,:10]
-Y = total.iloc[:,10]
-RF_Features_Importance(X,Y)
-"""
